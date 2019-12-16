f0db0c3882a999f8e1c7230f1b8c2fa7aa1bc6c5
Oleg Balunenko <obalunenko@luno.com>
2019-12-16 18:23:45 +0300

    puzzles/solutions: Fix doc

--- puzzles/solutions/doc.go
+++ puzzles/solutions/doc.go
@@ -1,21 +1,26 @@
 // Package declares common interface for puzzle solutions and functionality for register and run them.
-package solutions
-
+//
 // Each solution should implement `Solver` interface, be implemented udnder separate package and contain `init()`
 // function that will register that solution in list of all solvers.
-
+//
+// Example:
+//
 // 	type solver struct {
 //		name string
 //	}
 //
 //	func init() {
-// 		const puzzleName = "day01"
+// 		puzzleName, err := solutions.MakeName("2019", "day01")
+//		if err != nil {
+//			panic(err)
+//		}
 // 		puzzles.Register(puzzleName, solver{
 //		name: puzzleName,
 //		})
 // 	}
-
+//
 // Then to register solution in the list of all solutions: make a blank import of package with puzzle solution
 // at main.go
 //
-// _ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/day01"
+// 	import  _ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/day01"
+package solutions
