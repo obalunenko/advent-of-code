2e474d58fa09ca4b4c7f4ee99b2a00326f059876
Oleg Balunenko <obalunenko@luno.com>
2019-12-18 02:57:47 +0300

    puzzle/solutions: register 2019/day02

--- puzzles/solutions/register.go
+++ puzzles/solutions/register.go
@@ -1,5 +1,7 @@
 package solutions
 
 import (
+	// 2019 solutions
 	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day01" // register day01 solution
+	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day02" // register day02 solution
 )
