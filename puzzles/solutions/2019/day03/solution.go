f31f4664938efcac50160b36b63e79e0949ef139
Oleg Balunenko <obalunenko@luno.com>
2019-12-19 00:04:14 +0300

    2019/day03: Add spec and stub for puzzle

---
+++ puzzles/solutions/2019/day03/solution.go
@@ -0,0 +1,34 @@
+package day03
+
+import (
+	"io"
+
+	"github.com/oleg-balunenko/advent-of-code/puzzles"
+)
+
+func init() {
+	puzzleName, err := puzzles.MakeName("2019", "day03")
+	if err != nil {
+		panic(err)
+	}
+
+	puzzles.Register(puzzleName, solution{
+		name: puzzleName,
+	})
+}
+
+type solution struct {
+	name string
+}
+
+func (s solution) Part1(input io.Reader) (string, error) {
+	return "", puzzles.ErrNotImplemented
+}
+
+func (s solution) Part2(input io.Reader) (string, error) {
+	return "", puzzles.ErrNotImplemented
+}
+
+func (s solution) Name() string {
+	return s.name
+}
