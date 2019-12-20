c0e68e27ff65892384900964f76841be911858ca
Oleg Balunenko <obalunenko@luno.com>
2019-12-21 02:22:45 +0300

    2019/day03: Implement part1 solution for puzzle

--- puzzles/solutions/2019/day03/solution.go
+++ puzzles/solutions/2019/day03/solution.go
@@ -1,7 +1,13 @@
 package day03
 
 import (
+	"bufio"
 	"io"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
 
 	"github.com/oleg-balunenko/advent-of-code/puzzles"
 )
@@ -22,7 +28,32 @@ type solution struct {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	scanner := bufio.NewScanner(input)
+	res := make([]map[pos]bool, 0, 2)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		w := makeWire()
+		if err := w.run(line); err != nil {
+			return "", errors.Wrap(err, "failed to run wire")
+		}
+
+		res = append(res, w.m)
+	}
+
+	cross := findCross(res[0], res[1])
+
+	mds := make([]int, 0, len(cross))
+
+	for _, p := range cross {
+		md := p.manhattan()
+		mds = append(mds, md)
+	}
+
+	sort.Ints(mds)
+
+	return strconv.Itoa(mds[0]), nil
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
@@ -31,4 +62,114 @@ func (s solution) Part2(input io.Reader) (string, error) {
 
 func (s solution) Name() string {
 	return s.name
+}
+
+type wire struct {
+	pos pos
+	m   map[pos]bool
+}
+
+type pos struct {
+	x int
+	y int
+}
+
+func makeWire() wire {
+	return wire{
+		pos: pos{
+			x: 0,
+			y: 0,
+		},
+		m: make(map[pos]bool),
+	}
+}
+
+const (
+	moveUp    = "U"
+	moveDown  = "D"
+	moveRight = "R"
+	moveLeft  = "L"
+)
+
+func (w *wire) run(input string) error {
+	moves := strings.Split(input, ",")
+	for _, m := range moves {
+		move := m
+
+		act := string(move[0])
+
+		steps, err := strconv.Atoi(move[1:])
+		if err != nil {
+			return errors.New("failed to parse steps")
+		}
+
+		switch act {
+		case moveDown:
+			w.down(steps)
+		case moveUp:
+			w.up(steps)
+		case moveLeft:
+			w.left(steps)
+		case moveRight:
+			w.right(steps)
+		}
+	}
+
+	return nil
+}
+
+func (w *wire) up(n int) {
+	for i := 0; i < n; i++ {
+		w.pos.y++
+		w.m[w.pos] = true
+	}
+}
+
+func (w *wire) down(n int) {
+	for i := 0; i < n; i++ {
+		w.pos.y--
+		w.m[w.pos] = true
+	}
+}
+
+func (w *wire) left(n int) {
+	for i := 0; i < n; i++ {
+		w.pos.x--
+		w.m[w.pos] = true
+	}
+}
+
+func (w *wire) right(n int) {
+	for i := 0; i < n; i++ {
+		w.pos.x++
+		w.m[w.pos] = true
+	}
+}
+
+func (p pos) manhattan() int {
+	x, y := p.x, p.y
+
+	if x < 0 {
+		x = -x
+	}
+
+	if y < 0 {
+		y = -y
+	}
+
+	return x + y
+}
+
+func findCross(wm1 map[pos]bool, wm2 map[pos]bool) []pos {
+	res := make([]pos, 0, len(wm1))
+
+	for p := range wm1 {
+		p := p
+
+		if wm2[p] {
+			res = append(res, p)
+		}
+	}
+
+	return res
 }
--- puzzles/solutions/2019/day03/solution_test.go
+++ puzzles/solutions/2019/day03/solution_test.go
@@ -1,1 +1,128 @@
 package day03
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_solution_Part1(t *testing.T) {
+	type fields struct {
+		name string
+	}
+
+	type args struct {
+		input io.Reader
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "distance 6",
+			fields: fields{
+				name: "part1",
+			},
+			args: args{
+				input: strings.NewReader("U7,R6,D4,L4\n" +
+					"R8,U5,L5,D3"),
+			},
+			want:    "6",
+			wantErr: false,
+		},
+		{
+			name: "distance 159",
+			fields: fields{
+				name: "part1",
+			},
+			args: args{
+				input: strings.NewReader("R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
+					"U62,R66,U55,R34,D71,R55,D58,R83"),
+			},
+			want:    "159",
+			wantErr: false,
+		},
+		{
+			name: "distance 159",
+			fields: fields{
+				name: "part1",
+			},
+			args: args{
+				input: strings.NewReader("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
+					"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
+			},
+			want:    "135",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := solution{
+				name: tt.fields.name,
+			}
+
+			got, err := s.Part1(tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_findCross(t *testing.T) {
+	type args struct {
+		wm1 map[pos]bool
+		wm2 map[pos]bool
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []pos
+	}{
+		{
+			name: "",
+			args: args{
+				wm1: map[pos]bool{
+					{x: 0, y: 1}: true,
+					{x: 3, y: 1}: true,
+					{x: 4, y: 1}: true,
+					{x: 5, y: 2}: true,
+					{x: 6, y: 3}: true,
+				},
+				wm2: map[pos]bool{
+					{x: 0, y: 1}: true,
+					{x: 4, y: 1}: true,
+					{x: 6, y: 3}: true,
+				},
+			},
+			want: []pos{
+				{x: 0, y: 1},
+				{x: 4, y: 1},
+				{x: 6, y: 3},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := findCross(tt.args.wm1, tt.args.wm2)
+			assert.ElementsMatch(t, tt.want, got)
+		})
+	}
+}
