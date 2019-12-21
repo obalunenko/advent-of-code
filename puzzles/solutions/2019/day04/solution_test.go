ce062be3b297729666ed9fe7c07df5aa308181db
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-12-21 13:47:10 +0300

    2019/day04: Add spec and stubs for puzzle solution

---
+++ puzzles/solutions/2019/day04/solution_test.go
@@ -0,0 +1,95 @@
+package day04
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_solution_Part1(t *testing.T) {
+	type fields struct {
+		name string
+	}
+
+	type args struct {
+		input io.Reader
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "",
+			fields:  fields{},
+			args:    args{},
+			want:    "",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := solution{
+				name: tt.fields.name,
+			}
+
+			got, err := s.Part1(tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_solution_Part2(t *testing.T) {
+	type fields struct {
+		name string
+	}
+	type args struct {
+		input io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "",
+			fields:  fields{},
+			args:    args{},
+			want:    "",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := solution{
+				name: tt.fields.name,
+			}
+
+			got, err := s.Part2(tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
