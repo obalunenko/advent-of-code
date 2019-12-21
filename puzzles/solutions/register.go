8edfbb53290d981f14df549d369d9556ba6faddb
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-12-21 13:47:48 +0300

    puzzles/solutions: Register day04 solution

--- puzzles/solutions/register.go
+++ puzzles/solutions/register.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day01" // register day01 solution
 	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day02" // register day02 solution
 	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day03" // register day03 solution
+	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day04" // register day04 solution
 )
