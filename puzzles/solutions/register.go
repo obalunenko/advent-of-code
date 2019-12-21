040db38596073585d3656ce11024a0cebf771930
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-12-21 13:35:45 +0300

    Implement Day03 solution (#6)
    * 2019/day03: Add spec and stub for puzzle
    
    * puzzles/solutions: Register day03 puzzle
    
    * input/2019: Add input for day03 puzzle
    
    * cmd/aoc-cli: Sort imports
    
    * 2019/day02: Fix wsl linter
    
    * 2019/day02: Remove redundant log
    
    * Update README.md
    
    * puzzles/solver: Refactor Run to accept io.Reader; Add test
    
    * 2019/day02: Fix wsl linter
    
    * cmd/aoc-cli: Sort imports
    
    * Update travis config
    
    * 2019/day03: Add spec and stub for puzzle
    
    * puzzles/solutions: Register day03 puzzle
    
    * input/2019: Add input for day03 puzzle
    
    * 2019/day02: Remove redundant log
    
    * 2019/day03: Implement part1 solution for puzzle
    
    * 2019/day03: Add spec for part2
    
    * 2019/day03: Implement part2 solution for puzzle

--- puzzles/solutions/register.go
+++ puzzles/solutions/register.go
@@ -4,4 +4,5 @@ import (
 	// 2019 solutions
 	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day01" // register day01 solution
 	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day02" // register day02 solution
+	_ "github.com/oleg-balunenko/advent-of-code/puzzles/solutions/2019/day03" // register day03 solution
 )
