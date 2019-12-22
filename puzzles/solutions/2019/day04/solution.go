23316686e6710837774eed8d860ca6c6a8aa51f3
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-12-22 17:25:52 +0300

    2019/day04: Implement part1 solution

--- puzzles/solutions/2019/day04/solution.go
+++ puzzles/solutions/2019/day04/solution.go
@@ -1,7 +1,12 @@
 package day04
 
 import (
+	"bytes"
 	"io"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
 
 	"github.com/oleg-balunenko/advent-of-code/puzzles"
 )
@@ -22,7 +27,22 @@ type solution struct {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(input); err != nil {
+		return "", errors.Wrap(err, "failed to read")
+	}
+
+	limits := strings.Split(buf.String(), "-") // should be 2: low and high
+	if len(limits) != 2 {
+		return "", errors.New("invalid number of limits")
+	}
+
+	passwords, err := findPasswords(limits[0], limits[1])
+	if err != nil {
+		return "", errors.Wrap(err, "failed to find passwords")
+	}
+
+	return strconv.Itoa(passwords), nil
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
@@ -31,4 +51,67 @@ func (s solution) Part2(input io.Reader) (string, error) {
 
 func (s solution) Name() string {
 	return s.name
+}
+
+func findPasswords(low, high string) (int, error) {
+	lowd, err := strconv.Atoi(low)
+	if err != nil {
+		return -1, errors.Wrap(err, "failed to convert low to int")
+	}
+
+	highd, err := strconv.Atoi(high)
+	if err != nil {
+		return -1, errors.Wrap(err, "failed to convert high to int")
+	}
+
+	pwds := make([]int, 0, highd-lowd)
+
+	for i := lowd; i <= highd; i++ {
+		if isPassword(i) {
+			pwds = append(pwds, i)
+		}
+	}
+
+	return len(pwds), nil
+}
+
+func isIncreasing(n int) bool {
+	nmbs := intToSlice(n)
+
+	for i := 1; i <= len(nmbs)-1; i++ {
+		if nmbs[i] < nmbs[i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func hasDouble(n int) bool {
+	nmbs := intToSlice(n)
+
+	var hasDouble bool
+
+	for i := 1; i <= len(nmbs)-1; i++ {
+		if nmbs[i] == nmbs[i-1] {
+			hasDouble = true
+		}
+	}
+
+	return hasDouble
+}
+
+func isPassword(n int) bool {
+	return isIncreasing(n) && hasDouble(n)
+}
+
+func intToSlice(n int) [6]int {
+	return [6]int{
+		(n % 1000000) / 100000,
+		(n % 100000) / 10000,
+		(n % 10000) / 1000,
+		(n % 1000) / 100,
+		(n % 100) / 10,
+		n % 10,
+	}
 }
--- puzzles/solutions/2019/day04/solution_test.go
+++ puzzles/solutions/2019/day04/solution_test.go
@@ -2,6 +2,7 @@ package day04
 
 import (
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,10 +25,14 @@ func Test_solution_Part1(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "",
-			fields:  fields{},
-			args:    args{},
-			want:    "",
+			name: "",
+			fields: fields{
+				name: "",
+			},
+			args: args{
+				input: strings.NewReader("111000-111222"),
+			},
+			want:    "46",
 			wantErr: false,
 		},
 	}
@@ -52,43 +57,191 @@ func Test_solution_Part1(t *testing.T) {
 	}
 }
 
-func Test_solution_Part2(t *testing.T) {
-	type fields struct {
-		name string
-	}
+func Test_findPasswords(t *testing.T) {
 	type args struct {
-		input io.Reader
+		low  string
+		high string
 	}
+
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
-		want    string
+		want    int
 		wantErr bool
 	}{
 		{
-			name:    "",
-			fields:  fields{},
-			args:    args{},
-			want:    "",
+			name: "",
+			args: args{
+				low:  "111000",
+				high: "111222",
+			},
+			want:    46,
 			wantErr: false,
 		},
 	}
+
 	for _, tt := range tests {
 		tt := tt
 
 		t.Run(tt.name, func(t *testing.T) {
-			s := solution{
-				name: tt.fields.name,
-			}
-
-			got, err := s.Part2(tt.args.input)
+			got, err := findPasswords(tt.args.low, tt.args.high)
 			if tt.wantErr {
 				assert.Error(t, err)
 				return
 			}
 
 			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_isIncreasing(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{
+				n: 790768,
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				n: 123456,
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				n: 123450,
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := isIncreasing(tt.args.n)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_intToSlice(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want [6]int
+	}{
+		{
+			name: "",
+			args: args{
+				n: 123456,
+			},
+			want: [6]int{1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := intToSlice(tt.args.n)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_hasDouble(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{
+				n: 123456,
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				n: 122456,
+			},
+			want: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := hasDouble(tt.args.n)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_isPassword(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "meets these criteria (double `11`, never decreases)",
+			args: args{
+				n: 111111,
+			},
+			want: true,
+		},
+		{
+			name: "does not meet these criteria (decreasing pair of digits `50`)",
+			args: args{
+				n: 223450,
+			},
+			want: false,
+		},
+		{
+			name: "does not meet these criteria (no double).",
+			args: args{
+				n: 123789,
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := isPassword(tt.args.n)
 			assert.Equal(t, tt.want, got)
 		})
 	}
