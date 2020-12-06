24972e4a79d335705a675e4dea7fe45807c52bab
Oleg Balunenko <oleg.balunenko@gmail.com>
2020-12-06 22:07:56 +0300

    feat(): Add stab and input for implementation of 2020/day02 puzzle

---
+++ internal/puzzles/solutions/2020/day02/solution_test.go
@@ -0,0 +1,116 @@
+package day02
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_solution_Part1(t *testing.T) {
+	type fields struct {
+		year string
+		name string
+	}
+
+	type args struct {
+		input io.Reader
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test example from description",
+			fields: fields{
+				year: year,
+				name: puzzleName,
+			},
+			args: args{
+				input: strings.NewReader("1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"),
+			},
+			want:    "2",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := solution{
+				year: tt.fields.year,
+				name: tt.fields.name,
+			}
+			got, err := s.Part1(tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_solution_Part2(t *testing.T) {
+	t.Skip("not implemented yet")
+
+	type fields struct {
+		year string
+		name string
+	}
+
+	type args struct {
+		input io.Reader
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test example from description",
+			fields: fields{
+				year: year,
+				name: puzzleName,
+			},
+			args: args{
+				input: strings.NewReader(""),
+			},
+			want:    "",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := solution{
+				year: tt.fields.year,
+				name: tt.fields.name,
+			}
+
+			got, err := s.Part2(tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
