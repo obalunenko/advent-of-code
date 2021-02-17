8eda8d392f1865a744aab478f8d10c8eabbb9001
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-02-17 17:47:11 +0300

    feat(): Migrate to go 1.16 embed FS

---
+++ internal/input/content.go
@@ -0,0 +1,22 @@
+package input
+
+import (
+	"embed"
+	"path/filepath"
+)
+
+const (
+	dir = "data"
+)
+
+// content holds our puzzles inputs content.
+//go:embed data/*
+var content embed.FS
+
+// Asset loads and returns the asset for the given name.
+// It returns an error if the asset could not be found or
+// could not be loaded.
+func Asset(name string) ([]byte, error) {
+	return content.ReadFile(filepath.Clean(
+		filepath.Join(dir, name)))
+}
