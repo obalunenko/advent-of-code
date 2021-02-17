960486493d4029b158e973176afd76c25593d5d7
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-02-18 01:38:51 +0300

    chore(ci): Fix config

--- scripts/local-snapshot-release.sh
+++ scripts/local-snapshot-release.sh
@@ -5,9 +5,10 @@ set -e
 git fetch --tags
 
 # Get the latest tag name
+# shellcheck disable=SC2046
 latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo ${latestTag}
+echo "${latestTag}"
 
 export GOVERSION=$(go version | awk '{print $3;}')
 
-curl -sL https://git.io/goreleaser | bash
+goreleaser --snapshot --skip-publish --rm-dist
