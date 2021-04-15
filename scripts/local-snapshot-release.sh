26cc649b0d7082a4595db46941fef769bcae22f4
Oleg Balunenko <o.balunenko@melsoft-games.com>
2021-04-15 18:12:31 +0300

    chore(scripts): Remove unused variables

--- scripts/local-snapshot-release.sh
+++ scripts/local-snapshot-release.sh
@@ -12,7 +12,6 @@ trap cleanup SIGINT SIGTERM ERR EXIT
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 REPO_ROOT="$(cd ${SCRIPT_DIR} && git rev-parse --show-toplevel)"
-BIN_DIR=${REPO_ROOT}/bin
 
 echo "${SCRIPT_NAME} is running... "
 
--- scripts/vet.sh
+++ scripts/vet.sh
@@ -12,6 +12,7 @@ trap cleanup SIGINT SIGTERM ERR EXIT
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 REPO_ROOT="$(cd ${SCRIPT_DIR} && git rev-parse --show-toplevel)"
-TOOLS_DIR=${REPO_ROOT}/tools
+
+echo "${SCRIPT_NAME} is running... "
 
 go vet $(go list ./...)
