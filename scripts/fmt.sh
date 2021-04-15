52f718508085340693ca6cc7576bd0759bbf93c3
Oleg Balunenko <o.balunenko@melsoft-games.com>
2021-04-15 18:22:25 +0300

    chore(scripts): Update scripts

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -10,10 +10,9 @@ function cleanup() {
 trap cleanup SIGINT SIGTERM ERR EXIT
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 
 echo "${SCRIPT_NAME} is running... "
 
-gofmt -s -w -l $(find . -type f -name '*.go' | grep -v 'vendor' |grep -v '.git' )
+gofmt -s -w -l $(find . -type f -name '*.go' | grep -v 'vendor' | grep -v '.git' )
 
 echo "Done."
