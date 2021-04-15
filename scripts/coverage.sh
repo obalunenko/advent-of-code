60aa8d3a662b130622389893e5810d9c7b782c4f
Oleg Balunenko <o.balunenko@melsoft-games.com>
2021-04-15 18:22:25 +0300

    chore(scripts): Update scripts

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -11,7 +11,6 @@ function cleanup() {
 trap cleanup SIGINT SIGTERM ERR EXIT
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 
 echo "${SCRIPT_NAME} is running... "
 
