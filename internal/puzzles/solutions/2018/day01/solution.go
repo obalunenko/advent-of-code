13412e759ebfc7496ed86c26f25291f11f32151e
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-11-04 03:14:57 +0300

    feat(puzzles): Implement 2018/day02 part2

--- internal/puzzles/solutions/2018/day01/solution.go
+++ internal/puzzles/solutions/2018/day01/solution.go
@@ -1,7 +1,12 @@
 package day01
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
+	"regexp"
+	"strconv"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
 )
@@ -32,9 +37,121 @@ func init() {
 }
 
 func (s solution) Part1(in io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	return part1(in)
 }
 
 func (s solution) Part2(in io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	return part2(in)
+}
+
+var (
+	re = regexp.MustCompile(`(?s)(?P<sign>[+-])(?P<digits>\d+)`)
+)
+
+const (
+	_ = iota
+	sign
+	digits
+
+	totalmatches = 3
+)
+
+func part1(in io.Reader) (string, error) {
+	scanner := bufio.NewScanner(in)
+
+	var curfreq int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		matches := re.FindStringSubmatch(line)
+
+		if len(matches) != totalmatches {
+			return "", fmt.Errorf("wrong matches[%d] for line[%s], should be [%d]",
+				len(matches), line, totalmatches)
+		}
+
+		d, err := strconv.Atoi(matches[digits])
+		if err != nil {
+			return "", fmt.Errorf("strconv atoi: %w", err)
+		}
+
+		switch matches[sign] {
+		case "+":
+			curfreq += d
+		case "-":
+			curfreq -= d
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("scanner error: %w", err)
+	}
+
+	return strconv.Itoa(curfreq), nil
+}
+
+func part2(in io.Reader) (string, error) {
+	var buf bytes.Buffer
+
+	if _, err := buf.ReadFrom(in); err != nil {
+		return "", fmt.Errorf("failed to read: %w", err)
+	}
+
+	b := buf.Bytes()
+
+	seenfreqs := make(map[int]bool)
+
+	var (
+		curfreq int
+		loops   int
+		found   bool
+	)
+
+	for !found {
+		if len(seenfreqs) == 0 {
+			seenfreqs[curfreq] = true
+		}
+
+		scanner := bufio.NewScanner(bytes.NewReader(b))
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			matches := re.FindStringSubmatch(line)
+
+			if len(matches) != totalmatches {
+				return "", fmt.Errorf("wrong matches[%d] for line[%s], should be [%d]",
+					len(matches), line, totalmatches)
+			}
+
+			d, err := strconv.Atoi(matches[digits])
+			if err != nil {
+				return "", fmt.Errorf("strconv atoi: %w", err)
+			}
+
+			switch matches[sign] {
+			case "+":
+				curfreq += d
+			case "-":
+				curfreq -= d
+			}
+
+			if seenfreqs[curfreq] {
+				found = true
+
+				break
+			}
+
+			seenfreqs[curfreq] = true
+		}
+
+		if err := scanner.Err(); err != nil {
+			return "", fmt.Errorf("scanner error: %w", err)
+		}
+
+		loops++
+	}
+
+	return strconv.Itoa(curfreq), nil
 }
