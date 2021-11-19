2ff12eedc1a751b83075b5747014d327de9ab580
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-11-19 03:26:47 +0300

    feat: Implement 2017/day02 part1

--- internal/puzzles/solutions/2017/day02/solution.go
+++ internal/puzzles/solutions/2017/day02/solution.go
@@ -2,10 +2,21 @@
 package day02
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
+
+	log "github.com/obalunenko/logger"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
 )
+
+func init() {
+	puzzles.Register(solution{})
+}
 
 type solution struct{}
 
@@ -18,7 +29,51 @@ func (s solution) Year() string {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	scanner := bufio.NewScanner(input)
+
+	var checksum int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		line = strings.ReplaceAll(line, "\t", " ")
+
+		var min, max int
+
+		numbers := strings.Split(line, " ")
+
+		for i, number := range numbers {
+			d, err := strconv.Atoi(number)
+			if err != nil {
+				return "", fmt.Errorf("atoi: %w", err)
+			}
+
+			if i == 0 {
+				min, max = d, d
+			}
+
+			log.WithFields(context.TODO(), log.Fields{
+				"checksum": checksum,
+				"min":      min,
+				"max":      max,
+				"i":        i,
+				"number":   d,
+				"row":      line,
+			}).Info("Iterate")
+
+			if d < min {
+				min = d
+			}
+
+			if d > max {
+				max = d
+			}
+		}
+
+		checksum += max - min
+
+	}
+	return strconv.Itoa(checksum), nil
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
