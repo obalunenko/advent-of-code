f45a2d32dd46d9b52954fc96b7a576bb10c57fa8
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-02 01:47:01 +0300

    chore: Run only regression scope tests fo regression

--- scripts/tests/run-regression.sh
+++ scripts/tests/run-regression.sh
@@ -8,12 +8,14 @@ echo "${SCRIPT_NAME} is running... "
 
 export AOC_REGRESSION_ENABLED=true
 
+REGRESSION_TESTS_PKG=$(go list -m)/tests
+
 GOTEST="go test -v -race"
 if [ -f "$(go env GOPATH)/bin/gotestsum" ] || [ -f "/usr/local/bin/gotestsum" ]; then
   GOTEST="gotestsum --format pkgname --"
 fi
 
-${GOTEST} ./...
+${GOTEST} "${REGRESSION_TESTS_PKG}"
 
 unset AOC_REGRESSION_ENABLED
 
