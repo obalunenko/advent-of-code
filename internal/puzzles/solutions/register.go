0ba6bbfc1414d92be0f190ca7f3064e7b1cd983b
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-02 17:19:34 +0300

    feat: Initial commit for 2021/day01 puzzle

--- internal/puzzles/solutions/register.go
+++ internal/puzzles/solutions/register.go
@@ -47,4 +47,9 @@ import (
 	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2020/day01"
 	// register day02 solution.
 	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2020/day02"
+	/*
+		2021 solutions.
+	*/
+	// register day01 solution.
+	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2021/day01"
 )
