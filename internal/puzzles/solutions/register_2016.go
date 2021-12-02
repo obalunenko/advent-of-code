cd43e7ba1e3853aa712139887cccfcdc87981ee9
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-02 17:40:08 +0300

    refactor: register puzzles in each year file

---
+++ internal/puzzles/solutions/register_2016.go
@@ -0,0 +1,11 @@
+package solutions
+
+import (
+	/*
+		2016 solutions.
+	*/
+	// register day01 solution.
+	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2016/day01"
+	// register day02 solution.
+	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2016/day02"
+)
