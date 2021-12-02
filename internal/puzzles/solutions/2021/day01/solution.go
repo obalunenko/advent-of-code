8db35e778fded58ec874c3721b709d52c55e7e3f
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-02 17:27:15 +0300

    feat: Implement 2021/day01 part1

--- internal/puzzles/solutions/2021/day01/solution.go
+++ internal/puzzles/solutions/2021/day01/solution.go
@@ -2,7 +2,10 @@
 package day01
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
 )
@@ -22,7 +25,42 @@ func (s solution) Day() string {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	scanner := bufio.NewScanner(input)
+
+	var (
+		increasednum int
+		prev         int
+		idx          int
+	)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			return "", fmt.Errorf("atoi: %w", err)
+		}
+
+		if idx == 0 {
+			prev = n
+			idx++
+
+			continue
+		}
+
+		if n > prev {
+			increasednum++
+		}
+
+		prev = n
+		idx++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("scanner error: %w", err)
+	}
+
+	return strconv.Itoa(increasednum), nil
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
