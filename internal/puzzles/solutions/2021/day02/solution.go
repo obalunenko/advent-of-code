73b863ead367b47cbdac9de8c833057710b0f3c5
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-03 20:18:25 +0300

    feat: add 2021/day02 puzzle specs and tests

---
+++ internal/puzzles/solutions/2021/day02/solution.go
@@ -0,0 +1,30 @@
+// Package day02 contains solution for https://adventofcode.com/2021/day/2 puzzle.
+package day02
+
+import (
+	"io"
+
+	"github.com/obalunenko/advent-of-code/internal/puzzles"
+)
+
+func init() {
+	puzzles.Register(solution{})
+}
+
+type solution struct{}
+
+func (s solution) Year() string {
+	return puzzles.Year2021.String()
+}
+
+func (s solution) Day() string {
+	return puzzles.Day02.String()
+}
+
+func (s solution) Part1(input io.Reader) (string, error) {
+	return "", puzzles.ErrNotImplemented
+}
+
+func (s solution) Part2(input io.Reader) (string, error) {
+	return "", puzzles.ErrNotImplemented
+}
