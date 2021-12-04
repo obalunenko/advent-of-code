a1996055cdcf15172279aeb00dca53574f9c32a2
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-05 00:50:38 +0300

    feat: Implement 2021/day03 part1

--- internal/puzzles/solutions/2021/day03/solution.go
+++ internal/puzzles/solutions/2021/day03/solution.go
@@ -2,7 +2,10 @@
 package day03
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
 )
@@ -22,9 +25,100 @@ func (s solution) Day() string {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	diagnostic, err := makeDiagnostic(input)
+	if err != nil {
+		return "", fmt.Errorf("make diagnostic report: %w", err)
+	}
+
+	r := findRates(diagnostic)
+
+	consumption, err := r.consumption()
+	if err != nil {
+		return "", fmt.Errorf("consumption calculate: %w", err)
+	}
+
+	return consumption, nil
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
 	return "", puzzles.ErrNotImplemented
+}
+
+func makeDiagnostic(input io.Reader) ([]string, error) {
+	scanner := bufio.NewScanner(input)
+
+	var report []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		report = append(report, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+
+	return report, nil
+}
+
+type bitrates struct {
+	gamma   string
+	epsilon string
+}
+
+func (b bitrates) consumption() (string, error) {
+	g, err := strconv.ParseInt(b.gamma, 2, 64)
+	if err != nil {
+		return "", fmt.Errorf("parse gamma: %w", err)
+	}
+
+	e, err := strconv.ParseInt(b.epsilon, 2, 64)
+	if err != nil {
+		return "", fmt.Errorf("parse epsilon: %w", err)
+	}
+
+	c := g * e
+
+	return strconv.FormatInt(c, 10), nil
+}
+
+func findRates(diagnostic []string) bitrates {
+	var result bitrates
+
+	type (
+		bit string
+		pos int
+	)
+
+	const (
+		bit0 bit = "0"
+		bit1 bit = "1"
+	)
+
+	bitsStat := make(map[pos]map[bit]int)
+
+	for _, s := range diagnostic {
+		for p, r := range []rune(s) {
+			if bitsStat[pos(p)] == nil {
+				bitsStat[pos(p)] = make(map[bit]int)
+			}
+
+			bitsStat[pos(p)][bit(r)]++
+		}
+	}
+
+	for i := 0; i < len(bitsStat); i++ {
+		m := bitsStat[pos(i)]
+
+		if m[bit1] > m[bit0] {
+			result.gamma += string(bit1)
+			result.epsilon += string(bit0)
+		} else {
+			result.gamma += string(bit0)
+			result.epsilon += string(bit1)
+		}
+	}
+
+	return result
 }
--- internal/puzzles/solutions/2021/day03/solution_test.go
+++ internal/puzzles/solutions/2021/day03/solution_test.go
@@ -1,6 +1,7 @@
 package day03
 
 import (
+	"fmt"
 	"io"
 	"strings"
 	"testing"
@@ -37,7 +38,7 @@ func Test_solution_Part1(t *testing.T) {
 		name    string
 		args    args
 		want    string
-		wantErr bool
+		wantErr assert.ErrorAssertionFunc
 	}{
 		{
 			name: "test example from description",
@@ -45,20 +46,17 @@ func Test_solution_Part1(t *testing.T) {
 				input: strings.NewReader("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n"),
 			},
 			want:    "198",
-			wantErr: false,
+			wantErr: assert.NoError,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := s.Part1(tt.args.input)
-			if tt.wantErr {
-				assert.Error(t, err)
-
+			if !tt.wantErr(t, err, fmt.Sprintf("s.Part1()")) {
 				return
 			}
 
-			assert.NoError(t, err)
 			assert.Equal(t, tt.want, got)
 		})
 	}
@@ -75,7 +73,7 @@ func Test_solution_Part2(t *testing.T) {
 		name    string
 		args    args
 		want    string
-		wantErr bool
+		wantErr assert.ErrorAssertionFunc
 	}{
 		{
 			name: "test example from description",
@@ -83,21 +81,95 @@ func Test_solution_Part2(t *testing.T) {
 				input: strings.NewReader(""),
 			},
 			want:    "",
-			wantErr: false,
+			wantErr: assert.NoError,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := s.Part2(tt.args.input)
-			if tt.wantErr {
-				assert.Error(t, err)
-
+			if !tt.wantErr(t, err) {
 				return
 			}
 
-			assert.NoError(t, err)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_findRates(t *testing.T) {
+	type args struct {
+		diagnostic []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bitrates
+	}{
+		{
+			name: "",
+			args: args{
+				diagnostic: []string{
+					"00100",
+					"11110",
+					"10110",
+					"10111",
+					"10101",
+					"01111",
+					"00111",
+					"11100",
+					"10000",
+					"11001",
+					"00010",
+					"01010",
+				},
+			},
+			want: bitrates{
+				gamma:   "10110",
+				epsilon: "01001",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, findRates(tt.args.diagnostic), "findGammaRate(%v)", tt.args.diagnostic)
+		})
+	}
+}
+
+func Test_bitrates_consumption(t *testing.T) {
+	type fields struct {
+		gamma   string
+		epsilon string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "",
+			fields: fields{
+				gamma:   "10110",
+				epsilon: "01001",
+			},
+			want:    "198",
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := bitrates{
+				gamma:   tt.fields.gamma,
+				epsilon: tt.fields.epsilon,
+			}
+			got, err := b.consumption()
+			if !tt.wantErr(t, err, fmt.Sprintf("consumption()")) {
+				return
+			}
+
+			assert.Equalf(t, tt.want, got, "consumption()")
 		})
 	}
 }
