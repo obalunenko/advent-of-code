632011b9e0d0d3b30a21ef6b154c6612c6af07b8
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-05 02:53:59 +0300

    tests: Add regression tests for 2021/day03

--- tests/regression_test.go
+++ tests/regression_test.go
@@ -2288,8 +2288,8 @@ func testcases2021() []testcase {
 			want: puzzles.Result{
 				Year:  year,
 				Name:  puzzles.Day03.String(),
-				Part1: "",
-				Part2: "",
+				Part1: "4103154",
+				Part2: "4245351",
 			},
 			wantErr: false,
 		},
