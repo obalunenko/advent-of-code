cc83c0a5f184d16fdecd32776f96aca3651bcf05
Oleg Balunenko <o.balunenko@melsoft-games.com>
2021-12-09 02:30:07 +0300

    feat: Implement bingo game constructor with test

--- internal/puzzles/solutions/2021/day04/solution.go
+++ internal/puzzles/solutions/2021/day04/solution.go
@@ -2,7 +2,11 @@
 package day04
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"regexp"
+	"strconv"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
 )
@@ -22,9 +26,105 @@ func (s solution) Day() string {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
+	_, err := newBingoGame(input)
+	if err != nil {
+		return "", fmt.Errorf("new bingo game: %w", err)
+	}
+
 	return "", puzzles.ErrNotImplemented
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
 	return "", puzzles.ErrNotImplemented
+}
+
+type bingo struct {
+	input  []int
+	boards []*board
+}
+
+type board struct {
+	numbers [5][5]int
+}
+
+type inputType int
+
+const (
+	_ inputType = iota
+	inputNums
+	boardLine
+	emptyLine
+)
+
+func newBingoGame(input io.Reader) (*bingo, error) {
+	scanner := bufio.NewScanner(input)
+
+	var bg bingo
+
+	var (
+		idx       int
+		boardsNum int
+		cursor    int
+	)
+
+	for scanner.Scan() {
+		it := boardLine
+
+		if idx == 0 {
+			it = inputNums
+		}
+
+		line := scanner.Text()
+		if line == "" {
+			it = emptyLine
+		}
+
+		numbers, err := getNumbers(line)
+		if err != nil {
+			return nil, fmt.Errorf("get numbers: %w", err)
+		}
+
+		switch it {
+		case inputNums:
+			bg.input = numbers
+		case emptyLine:
+			bg.boards = append(bg.boards, &board{
+				numbers: [5][5]int{},
+			})
+
+			boardsNum++
+			cursor = 0
+		case boardLine:
+			for i, n := range numbers {
+				bg.boards[boardsNum-1].numbers[cursor][i] = n
+			}
+
+			cursor++
+		}
+
+		idx++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+
+	return &bg, nil
+}
+
+var re = regexp.MustCompile(`(?s)\d+`)
+
+func getNumbers(s string) ([]int, error) {
+	var nums []int
+
+	for _, match := range re.FindAllString(s, -1) {
+		n, err := strconv.Atoi(match)
+		if err != nil {
+			return nil, fmt.Errorf("parse num: %w", err)
+		}
+
+		nums = append(nums, n)
+	}
+
+	return nums, nil
 }
--- internal/puzzles/solutions/2021/day04/solution_test.go
+++ internal/puzzles/solutions/2021/day04/solution_test.go
@@ -1,6 +1,7 @@
 package day04
 
 import (
+	"fmt"
 	"io"
 	"path/filepath"
 	"testing"
@@ -94,6 +95,70 @@ func Test_solution_Part2(t *testing.T) {
 			}
 
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_newBingoGame(t *testing.T) {
+	type args struct {
+		input io.Reader
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *bingo
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "",
+			args: args{
+				input: utils.ReaderFromFile(t, filepath.Join("testdata", "input.txt")),
+			},
+			want: &bingo{
+				input: []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1},
+				boards: []*board{
+					{
+						numbers: [5][5]int{
+							{22, 13, 17, 11, 0},
+							{8, 2, 23, 4, 24},
+							{21, 9, 14, 16, 7},
+							{6, 10, 3, 18, 5},
+							{1, 12, 20, 15, 19},
+						},
+					},
+					{
+						numbers: [5][5]int{
+							{3, 15, 0, 2, 22},
+							{9, 18, 13, 17, 5},
+							{19, 8, 7, 25, 23},
+							{20, 11, 10, 24, 4},
+							{14, 21, 16, 12, 6},
+						},
+					},
+					{
+						numbers: [5][5]int{
+							{14, 21, 17, 24, 4},
+							{10, 16, 15, 9, 19},
+							{18, 8, 23, 26, 20},
+							{22, 11, 13, 6, 5},
+							{2, 0, 12, 3, 7},
+						},
+					},
+				},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := newBingoGame(tt.args.input)
+			if !tt.wantErr(t, err, fmt.Sprintf("newBingoGame(%v)", tt.args.input)) {
+				return
+			}
+			
+			assert.Equalf(t, tt.want, got, "newBingoGame(%v)", tt.args.input)
 		})
 	}
 }
