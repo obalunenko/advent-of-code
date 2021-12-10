fa783458383c1623c003584688909c266fa5fa5b
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-10 19:47:36 +0300

    chore: Update test run scripts

--- scripts/tests/run-regression.sh
+++ scripts/tests/run-regression.sh
@@ -10,12 +10,12 @@ export AOC_REGRESSION_ENABLED=true
 
 REGRESSION_TESTS_PKG=$(go list -m)/tests
 
-GOTEST="go test -v -race"
-if [ -f "$(go env GOPATH)/bin/gotestsum" ] || [ -f "/usr/local/bin/gotestsum" ]; then
+GOTEST="go test "
+if command -v "gotestsum" &>/dev/null; then
   GOTEST="gotestsum --format pkgname --"
 fi
 
-${GOTEST} "${REGRESSION_TESTS_PKG}"
+${GOTEST} -race "${REGRESSION_TESTS_PKG}"
 
 unset AOC_REGRESSION_ENABLED
 
