3d1bc0de22235b35aaba0973172b9b2055876d84
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-12 15:25:13 +0300

    feat: Implement 2021/day05 part1

--- internal/puzzles/solutions/2021/day05/solution.go
+++ internal/puzzles/solutions/2021/day05/solution.go
@@ -2,7 +2,13 @@
 package day05
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
 )
@@ -22,9 +28,280 @@ func (s solution) Day() string {
 }
 
 func (s solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	lines, err := getLines(input)
+	if err != nil {
+		return "", fmt.Errorf("get lines: %w", err)
+	}
+
+	lines = filterLines(lines, part1Filter)
+
+	d := drawDiagram(lines)
+
+	zones := d.dangerZones(part1DangerZone)
+
+	return strconv.Itoa(zones), nil
 }
 
 func (s solution) Part2(input io.Reader) (string, error) {
 	return "", puzzles.ErrNotImplemented
+}
+
+type position struct {
+	x, y int
+}
+
+type line struct {
+	start position
+	end   position
+}
+
+type diagram struct {
+	data [][]int
+}
+
+type dangerFunc func(n int) bool
+
+func part1DangerZone(n int) bool {
+	return n > 1
+}
+
+func (d diagram) dangerZones(f dangerFunc) int {
+	var zones int
+
+	for _, xs := range d.data {
+		for _, x := range xs {
+			if f(x) {
+				zones++
+			}
+		}
+	}
+
+	return zones
+}
+
+func (d diagram) String() string {
+	const (
+		empty   = "."
+		newline = "\n"
+	)
+
+	var res string
+
+	last := len(d.data) - 1
+
+	for i := 0; i <= last; i++ {
+		xs := d.data[i]
+
+		for _, x := range xs {
+			if x == 0 {
+				res += empty
+
+				continue
+			}
+
+			res += strconv.Itoa(x)
+		}
+
+		if i != last {
+			res += newline
+		}
+	}
+
+	return res
+}
+
+func (d *diagram) draw(lines []line) {
+	for _, l := range lines {
+		if l.start.x == l.end.x {
+			d.drawY(l)
+		}
+
+		if l.start.y == l.end.y {
+			d.drawX(l)
+		}
+	}
+}
+
+func (d *diagram) drawX(l line) {
+	y := l.start.y
+
+	x1, x2 := l.start.x, l.end.x
+	if x1 > x2 {
+		x1, x2 = x2, x1
+	}
+
+	for i := x1; i <= x2; i++ {
+		d.data[y][i]++
+	}
+}
+
+func (d *diagram) drawY(l line) {
+	x := l.start.x
+
+	y1, y2 := l.start.y, l.end.y
+	if y1 > y2 {
+		y1, y2 = y2, y1
+	}
+
+	for i := y1; i <= y2; i++ {
+		d.data[i][x]++
+	}
+}
+
+func newDiagram(maxX, maxY int) diagram {
+	res := make([][]int, maxY+1)
+
+	for i := 0; i < maxY+1; i++ {
+		res[i] = make([]int, maxX+1)
+	}
+
+	return diagram{
+		data: res,
+	}
+}
+
+func drawDiagram(lines []line) diagram {
+
+	// get max x,y
+	bounds := getBounds(lines)
+
+	// allocate
+	d := newDiagram(bounds.x, bounds.y)
+
+	// draw
+	d.draw(lines)
+
+	return d
+}
+
+func getBounds(lines []line) position {
+	var (
+		maxX, maxY int
+	)
+
+	for _, l := range lines {
+		if l.start.x > maxX {
+			maxX = l.start.x
+		}
+
+		if l.start.y > maxY {
+			maxY = l.start.y
+		}
+
+		if l.end.x > maxX {
+			maxX = l.end.x
+		}
+
+		if l.end.y > maxY {
+			maxY = l.end.y
+		}
+	}
+
+	return position{
+		x: maxX,
+		y: maxY,
+	}
+}
+
+func getLines(input io.Reader) ([]line, error) {
+	scanner := bufio.NewScanner(input)
+
+	var lines []line
+
+	const (
+		startpos = 0
+		endpos   = 1
+	)
+
+	for scanner.Scan() {
+		l := scanner.Text()
+
+		coordinates, err := parseLine(l)
+		if err != nil {
+			return nil, fmt.Errorf("get numbers: %w", err)
+		}
+
+		lines = append(lines, line{
+			start: coordinates[startpos],
+			end:   coordinates[endpos],
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+
+	return lines, nil
+}
+
+var reg = regexp.MustCompile(`(?s)\d+,\d+`)
+
+func parseLine(line string) ([]position, error) {
+	const (
+		matchNum = 2
+	)
+
+	res := make([]position, 0, matchNum)
+
+	matches := reg.FindAllString(line, -1)
+	if len(matches) != matchNum {
+		return nil, errors.New("wrong coordinates line")
+	}
+
+	for i := range matches {
+		coordinates, err := parseCoordinates(matches[i])
+		if err != nil {
+			return nil, fmt.Errorf("parse coordinates: %w", err)
+		}
+
+		res = append(res, coordinates)
+	}
+
+	return res, nil
+}
+
+func parseCoordinates(s string) (position, error) {
+	const (
+		cNum  = 2
+		delim = ","
+		xpos  = 0
+		ypos  = 1
+	)
+
+	spl := strings.Split(s, delim)
+	if len(spl) != cNum {
+		return position{}, errors.New("wrong coordinates pair")
+	}
+
+	x, err := strconv.Atoi(spl[xpos])
+	if err != nil {
+		return position{}, fmt.Errorf("parse x to int: %w", err)
+	}
+
+	y, err := strconv.Atoi(spl[ypos])
+	if err != nil {
+		return position{}, fmt.Errorf("parse y to int: %w", err)
+	}
+
+	return position{
+		x: x,
+		y: y,
+	}, nil
+}
+
+type filterFunc func(l line) bool
+
+func part1Filter(l line) bool {
+	return l.start.x == l.end.x || l.start.y == l.end.y
+}
+
+func filterLines(lines []line, filter filterFunc) []line {
+	filtered := lines[:0]
+	for _, x := range lines {
+		if filter(x) {
+			filtered = append(filtered, x)
+		}
+	}
+
+	return filtered
 }
--- internal/puzzles/solutions/2021/day05/solution_test.go
+++ internal/puzzles/solutions/2021/day05/solution_test.go
@@ -1,11 +1,13 @@
 package day05
 
 import (
+	"fmt"
 	"io"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles/common/utils"
 )
@@ -94,6 +96,501 @@ func Test_solution_Part2(t *testing.T) {
 			}
 
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_parseLine(t *testing.T) {
+	type args struct {
+		line string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []position
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "",
+			args: args{
+				line: "0,9 -> 5,9",
+			},
+			want: []position{
+				{
+					x: 0,
+					y: 9,
+				},
+				{
+					x: 5,
+					y: 9,
+				},
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "",
+			args: args{
+				line: "105,697 -> 287,697",
+			},
+			want: []position{
+				{
+					x: 105,
+					y: 697,
+				},
+				{
+					x: 287,
+					y: 697,
+				},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseLine(tt.args.line)
+			if !tt.wantErr(t, err, fmt.Sprintf("parseLine(%v)", tt.args.line)) {
+				return
+			}
+
+			assert.Equalf(t, tt.want, got, "parseLine(%v)", tt.args.line)
+		})
+	}
+}
+
+func Test_parseCoordinates(t *testing.T) {
+	type args struct {
+		s string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    position
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "",
+			args: args{
+				s: "0,9",
+			},
+			want: position{
+				x: 0,
+				y: 9,
+			},
+			wantErr: assert.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseCoordinates(tt.args.s)
+			if !tt.wantErr(t, err, fmt.Sprintf("parseCoordinates(%v)", tt.args.s)) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "parseCoordinates(%v)", tt.args.s)
+		})
+	}
+}
+
+func Test_getLines(t *testing.T) {
+	type args struct {
+		input io.Reader
+	}
+
+	var tests = []struct {
+		name    string
+		args    args
+		want    []line
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "",
+			args: args{
+				input: utils.ReaderFromFile(t, filepath.Join("testdata", "input.txt")),
+			},
+			want: []line{
+				{
+					start: position{x: 0, y: 9},
+					end:   position{x: 5, y: 9},
+				},
+				{
+					start: position{x: 8, y: 0},
+					end:   position{x: 0, y: 8},
+				},
+				{
+					start: position{x: 9, y: 4},
+					end:   position{x: 3, y: 4},
+				},
+				{
+					start: position{x: 2, y: 2},
+					end:   position{x: 2, y: 1},
+				},
+				{
+					start: position{x: 7, y: 0},
+					end:   position{x: 7, y: 4},
+				},
+				{
+					start: position{x: 6, y: 4},
+					end:   position{x: 2, y: 0},
+				},
+				{
+					start: position{x: 0, y: 9},
+					end:   position{x: 2, y: 9},
+				},
+				{
+					start: position{x: 3, y: 4},
+					end:   position{x: 1, y: 4},
+				},
+				{
+					start: position{x: 0, y: 0},
+					end:   position{x: 8, y: 8},
+				},
+				{
+					start: position{x: 5, y: 5},
+					end:   position{x: 8, y: 2},
+				},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getLines(tt.args.input)
+			if !tt.wantErr(t, err, fmt.Sprintf("getLines(%v)", tt.args.input)) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "getLines(%v)", tt.args.input)
+		})
+	}
+}
+
+func Test_newDiagram(t *testing.T) {
+	type args struct {
+		maxX int
+		maxY int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want diagram
+	}{
+		{
+			name: "",
+			args: args{
+				maxX: 3,
+				maxY: 4,
+			},
+			want: diagram{
+				data: [][]int{
+					{0, 0, 0, 0},
+					{0, 0, 0, 0},
+					{0, 0, 0, 0},
+					{0, 0, 0, 0},
+					{0, 0, 0, 0},
+				},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				maxX: 9,
+				maxY: 9,
+			},
+			want: diagram{
+				data: [][]int{
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, newDiagram(tt.args.maxX, tt.args.maxY), "newDiagram(%v, %v)", tt.args.maxX, tt.args.maxY)
+		})
+	}
+}
+
+func Test_filterLines(t *testing.T) {
+	type args struct {
+		lines  []line
+		filter filterFunc
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []line
+	}{
+		{
+			name: "",
+			args: args{
+				lines: []line{
+					{
+						start: position{x: 0, y: 9},
+						end:   position{x: 5, y: 9},
+					},
+					{
+						start: position{x: 8, y: 0},
+						end:   position{x: 0, y: 8},
+					},
+					{
+						start: position{x: 9, y: 4},
+						end:   position{x: 3, y: 4},
+					},
+					{
+						start: position{x: 2, y: 2},
+						end:   position{x: 2, y: 1},
+					},
+					{
+						start: position{x: 7, y: 0},
+						end:   position{x: 7, y: 4},
+					},
+					{
+						start: position{x: 6, y: 4},
+						end:   position{x: 2, y: 0},
+					},
+					{
+						start: position{x: 0, y: 9},
+						end:   position{x: 2, y: 9},
+					},
+					{
+						start: position{x: 3, y: 4},
+						end:   position{x: 1, y: 4},
+					},
+					{
+						start: position{x: 0, y: 0},
+						end:   position{x: 8, y: 8},
+					},
+					{
+						start: position{x: 5, y: 5},
+						end:   position{x: 8, y: 2},
+					},
+				},
+				filter: part1Filter,
+			},
+			want: []line{
+				{
+					start: position{x: 0, y: 9},
+					end:   position{x: 5, y: 9},
+				},
+				{
+					start: position{x: 9, y: 4},
+					end:   position{x: 3, y: 4},
+				},
+				{
+					start: position{x: 2, y: 2},
+					end:   position{x: 2, y: 1},
+				},
+				{
+					start: position{x: 7, y: 0},
+					end:   position{x: 7, y: 4},
+				},
+				{
+					start: position{x: 0, y: 9},
+					end:   position{x: 2, y: 9},
+				},
+				{
+					start: position{x: 3, y: 4},
+					end:   position{x: 1, y: 4},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, filterLines(tt.args.lines, tt.args.filter), "filterLines(%v, %v)", tt.args.lines, tt.args.filter)
+		})
+	}
+}
+
+func Test_diagram_String(t *testing.T) {
+	tests := []struct {
+		name string
+		d    diagram
+		want string
+	}{
+		{
+			name: "",
+			d: diagram{
+				data: [][]int{
+					{0, 1, 2, 0, 0},
+					{1, 0, 0, 0, 0},
+					{0, 1, 0, 2, 0},
+				},
+			},
+			want: ".12..\n" +
+				"1....\n" +
+				".1.2.",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.d.String(), "String()")
+		})
+	}
+}
+
+func Test_getBounds(t *testing.T) {
+	type args struct {
+		lines []line
+	}
+	tests := []struct {
+		name string
+		args args
+		want position
+	}{
+		{
+			name: "",
+			args: args{
+				lines: []line{
+					{
+						start: position{x: 0, y: 9},
+						end:   position{x: 5, y: 9},
+					},
+					{
+						start: position{x: 9, y: 4},
+						end:   position{x: 3, y: 4},
+					},
+					{
+						start: position{x: 2, y: 2},
+						end:   position{x: 2, y: 1},
+					},
+					{
+						start: position{x: 7, y: 0},
+						end:   position{x: 7, y: 4},
+					},
+					{
+						start: position{x: 0, y: 9},
+						end:   position{x: 2, y: 9},
+					},
+					{
+						start: position{x: 3, y: 4},
+						end:   position{x: 1, y: 4},
+					},
+				},
+			},
+			want: position{
+				x: 9,
+				y: 9,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, getBounds(tt.args.lines), "getBounds(%v)", tt.args.lines)
+		})
+	}
+}
+
+func Test_drawDiagram(t *testing.T) {
+	type args struct {
+		lines []line
+	}
+
+	tests := []struct {
+		name            string
+		args            args
+		wantDiagramPath string
+	}{
+		{
+			name: "",
+			args: args{
+				lines: []line{
+					{
+						start: position{x: 0, y: 9},
+						end:   position{x: 5, y: 9},
+					},
+					{
+						start: position{x: 9, y: 4},
+						end:   position{x: 3, y: 4},
+					},
+					{
+						start: position{x: 2, y: 2},
+						end:   position{x: 2, y: 1},
+					},
+					{
+						start: position{x: 7, y: 0},
+						end:   position{x: 7, y: 4},
+					},
+					{
+						start: position{x: 0, y: 9},
+						end:   position{x: 2, y: 9},
+					},
+					{
+						start: position{x: 3, y: 4},
+						end:   position{x: 1, y: 4},
+					},
+				},
+			},
+			wantDiagramPath: filepath.Join("testdata", "expected-diagram.txt"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			file := utils.ReaderFromFile(t, tt.wantDiagramPath)
+
+			content, err := io.ReadAll(file)
+			require.NoError(t, err)
+
+			assert.Equalf(t, string(content), drawDiagram(tt.args.lines).String(),
+				"drawDiagram(%v)", tt.args.lines)
+		})
+	}
+}
+
+func Test_diagram_dangerZones(t *testing.T) {
+	type fields struct {
+		data [][]int
+	}
+
+	type args struct {
+		f dangerFunc
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name: "",
+			fields: fields{
+				data: [][]int{
+					{0, 1, 2, 0, 0},
+					{1, 0, 0, 0, 0},
+					{0, 1, 0, 2, 0},
+				},
+			},
+			args: args{
+				f: part1DangerZone,
+			},
+			want: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := diagram{
+				data: tt.fields.data,
+			}
+			assert.Equalf(t, tt.want, d.dangerZones(tt.args.f), "dangerZones(%v)", tt.args.f)
 		})
 	}
 }
