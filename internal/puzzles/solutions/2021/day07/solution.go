e30a3e87361a0f2a419b5ba049cdda0e6769a375
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-12-17 17:12:01 +0300

    feat(2021/day07): Implement part1

--- internal/puzzles/solutions/2021/day07/solution.go
+++ internal/puzzles/solutions/2021/day07/solution.go
@@ -1,9 +1,14 @@
+// Package day07 contains solution for https://adventofcode.com/2021/day/7 puzzle.
 package day07
 
 import (
+	"fmt"
 	"io"
+	"sort"
+	"strconv"
 
 	"github.com/obalunenko/advent-of-code/internal/puzzles"
+	"github.com/obalunenko/advent-of-code/internal/puzzles/common/utils"
 )
 
 func init() {
@@ -21,9 +26,109 @@ func (solution) Year() string {
 }
 
 func (solution) Part1(input io.Reader) (string, error) {
-	return "", puzzles.ErrNotImplemented
+	crabs, err := getCrabs(input)
+	if err != nil {
+		return "", fmt.Errorf("get crabs: %w", err)
+	}
+
+	s := makeSwarm(crabs)
+
+	s.calcDistances()
+
+	cost := s.minDistanceCost()
+
+	return strconv.Itoa(cost), nil
 }
 
 func (solution) Part2(input io.Reader) (string, error) {
 	return "", puzzles.ErrNotImplemented
+}
+
+func getCrabs(input io.Reader) ([]int, error) {
+	crabs, err := utils.ParseInts(input, ",")
+	if err != nil {
+		return nil, fmt.Errorf("parse int slice from reader: %w", err)
+	}
+
+	sort.Ints(crabs)
+
+	return crabs, nil
+}
+
+const (
+	undef = -99999
+)
+
+func makeMatrix(crabs []int) [][]int {
+	const (
+		header    = 1
+		headerPos = 0 // matrix[y][x] - where 0 - x
+	)
+
+	cnum := len(crabs) + header
+
+	matrix := make([][]int, cnum)
+
+	// matrix[i][j]
+	for i := 0; i < cnum; i++ {
+		matrix[i] = make([]int, cnum)
+
+		for j := 0; j < cnum; j++ {
+			switch {
+			case i == headerPos && j == headerPos:
+				matrix[i][j] = undef
+			case i == headerPos:
+				matrix[i][j] = crabs[j-1]
+			case j == headerPos:
+				matrix[i][j] = crabs[i-1]
+			}
+		}
+	}
+
+	return matrix
+}
+
+type swarm struct {
+	crabsMatrix [][]int
+}
+
+func makeSwarm(crabs []int) swarm {
+	return swarm{
+		crabsMatrix: makeMatrix(crabs),
+	}
+}
+
+func (s *swarm) calcDistances() {
+	for i := 1; i < len(s.crabsMatrix); i++ {
+		for j := 1; j < len(s.crabsMatrix); j++ {
+			p := s.crabsMatrix[i][0] - s.crabsMatrix[0][j]
+			if p < 0 {
+				p *= -1
+			}
+
+			s.crabsMatrix[i][j] = p
+		}
+	}
+}
+
+func (s swarm) minDistanceCost() int {
+	var min int
+
+	for i := 1; i < len(s.crabsMatrix); i++ {
+		var f int
+
+		for j := 1; j < len(s.crabsMatrix); j++ {
+			f += s.crabsMatrix[i][j]
+		}
+
+		if i == 1 {
+			min = f
+		}
+
+		if f < min {
+			min = f
+		}
+	}
+
+	return min
 }
--- internal/puzzles/solutions/2021/day07/solution_test.go
+++ internal/puzzles/solutions/2021/day07/solution_test.go
@@ -113,3 +113,113 @@ func Test_solution_Part2(t *testing.T) {
 		})
 	}
 }
+
+func Test_makeMatrix(t *testing.T) {
+	type args struct {
+		crabs []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "",
+			args: args{
+				crabs: []int{
+					1, 45, 3, 4,
+				},
+			},
+			want: [][]int{
+				{undef, 1, 45, 3, 4},
+				{1, 0, 0, 0, 0},
+				{45, 0, 0, 0, 0},
+				{3, 0, 0, 0, 0},
+				{4, 0, 0, 0, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, makeMatrix(tt.args.crabs), "makeMatrix(%v)", tt.args.crabs)
+		})
+	}
+}
+
+func Test_swarm_calcDistances(t *testing.T) {
+	type fields struct {
+		crabsMatrix [][]int
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		expected *swarm
+	}{
+		{
+			name: "",
+			fields: fields{
+				crabsMatrix: [][]int{
+					{undef, 1, 2, 3, 4},
+					{1, 0, 0, 0, 0},
+					{2, 0, 0, 0, 0},
+					{3, 0, 0, 0, 0},
+					{4, 0, 0, 0, 0},
+				},
+			},
+			expected: &swarm{
+				crabsMatrix: [][]int{
+					{undef, 1, 2, 3, 4},
+					{1, 0, 1, 2, 3},
+					{2, 1, 0, 1, 2},
+					{3, 2, 1, 0, 1},
+					{4, 3, 2, 1, 0},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &swarm{
+				crabsMatrix: tt.fields.crabsMatrix,
+			}
+			s.calcDistances()
+
+			assert.Equal(t, tt.expected, s)
+		})
+	}
+}
+
+func Test_swarm_minDistanceCost(t *testing.T) {
+	type fields struct {
+		crabsMatrix [][]int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "",
+			fields: fields{
+				crabsMatrix: [][]int{
+					{undef, 1, 2, 3, 4},
+					{1, 0, 1, 2, 3},
+					{2, 1, 0, 1, 2},
+					{3, 2, 1, 0, 1},
+					{4, 3, 2, 1, 0},
+				},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := swarm{
+				crabsMatrix: tt.fields.crabsMatrix,
+			}
+
+			assert.Equalf(t, tt.want, s.minDistanceCost(), "minDistanceCost()")
+		})
+	}
+}
