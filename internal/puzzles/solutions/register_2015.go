750f237b3e72ef48444a1ae201d94fcbbe975abb
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-01-15 17:52:40 +0300

    feat(puzzles): Register 2015/day03 solver

--- internal/puzzles/solutions/register_2015.go
+++ internal/puzzles/solutions/register_2015.go
@@ -8,4 +8,6 @@ import (
 	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2015/day01"
 	// register day02 solution.
 	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2015/day02"
+	// register day03 solution.
+	_ "github.com/obalunenko/advent-of-code/internal/puzzles/solutions/2015/day03"
 )
