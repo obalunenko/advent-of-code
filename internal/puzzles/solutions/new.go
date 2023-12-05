2afa72a197bc97819571796d714aef759a1bc9eb
Oleg Balunenko <oleg.balunenko@gmail.com>
2023-12-05 22:16:57 +0400

    feat: Implement new puzzle solution generation

---
+++ internal/puzzles/solutions/new.go
@@ -0,0 +1,170 @@
+package solutions
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"text/template"
+
+	"github.com/obalunenko/advent-of-code/internal/puzzles/solutions/templates"
+)
+
+func createNewFromTemplate(purl string) error {
+	const (
+		perms   = 0o766
+		yearLen = 4
+		dayLen  = 2
+	)
+
+	pd, err := parsePuzzleURL(purl)
+	if err != nil {
+		return fmt.Errorf("parse puzzle url: %w", err)
+	}
+
+	day := strconv.Itoa(pd.day)
+	if len(day) < dayLen {
+		day = "0" + day
+	}
+
+	if len(day) != dayLen {
+		return fmt.Errorf("invalid day: %s", day)
+	}
+
+	year := strconv.Itoa(pd.year)
+
+	if len(year) != yearLen {
+		return fmt.Errorf("invalid year: %s", year)
+	}
+
+	params := templates.Params{
+		Year:   year,
+		Day:    pd.day,
+		DayStr: day,
+		URL:    purl,
+	}
+
+	path := filepath.Clean(filepath.Join(year, "day"+day))
+
+	if err = createPuzzleDir(path, perms); err != nil {
+		return fmt.Errorf("failed to create puzzle dir: %w", err)
+	}
+
+	testdata := filepath.Clean(filepath.Join(path, "testdata"))
+
+	if err = createTestdata(testdata, perms); err != nil {
+		return fmt.Errorf("failed to create testdata: %w", err)
+	}
+
+	tmplsFns := []func() (*template.Template, error){
+		templates.SolutionTmpl, templates.SolutionTestTmpl, templates.SpecTmpl,
+	}
+
+	for _, tmplFn := range tmplsFns {
+		var tmpl *template.Template
+
+		tmpl, err = tmplFn()
+		if err != nil {
+			return fmt.Errorf("failed to get template: %w", err)
+		}
+
+		if err = createFromTemplate(tmpl, path, perms, params); err != nil {
+			return fmt.Errorf("failed to create from template: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func createFromTemplate(tmpl *template.Template, path string, perms os.FileMode, params templates.Params) error {
+	fpath := filepath.Clean(filepath.Join(path, tmpl.Name()))
+
+	if isExist(fpath) {
+		return nil
+	}
+
+	var content []byte
+
+	content, err := templates.SubstituteTemplate(tmpl, params)
+	if err != nil {
+		return fmt.Errorf("failed to substitute template: %w", err)
+	}
+
+	if err = os.WriteFile(fpath, content, perms); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
+}
+
+func createPuzzleDir(path string, perms os.FileMode) error {
+	if !isExist(path) {
+		if err := os.MkdirAll(path, perms); err != nil {
+			return fmt.Errorf("failed to create dir: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func createTestdata(path string, perms os.FileMode) error {
+	if !isExist(path) {
+		if err := os.MkdirAll(path, perms); err != nil {
+			return fmt.Errorf("failed to create dir: %w", err)
+		}
+	}
+
+	input := filepath.Clean(filepath.Join(path, "input.txt"))
+
+	if !isExist(input) {
+		var f *os.File
+
+		f, err := os.Create(input)
+		if err != nil {
+			return fmt.Errorf("failed to create file: %w", err)
+		}
+
+		if err = f.Close(); err != nil {
+			return fmt.Errorf("failed to close file: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func isExist(path string) bool {
+	stat, err := os.Stat(path)
+	if err != nil && !os.IsNotExist(err) {
+		return false
+	}
+
+	return stat != nil && stat.Name() != ""
+}
+
+type puzzleDate struct {
+	year int
+	day  int
+}
+
+func parsePuzzleURL(url string) (puzzleDate, error) {
+	const (
+		urlFmt    = "https://adventofcode.com/%d/day/%d"
+		paramsNum = 2
+	)
+
+	var year, day int
+
+	n, err := fmt.Sscanf(url, urlFmt, &year, &day)
+	if err != nil {
+		return puzzleDate{}, fmt.Errorf("parse puzzle url: %w", err)
+	}
+
+	if n != paramsNum {
+		return puzzleDate{}, fmt.Errorf("invalid puzzle url: %s", url)
+	}
+
+	return puzzleDate{
+		year: year,
+		day:  day,
+	}, nil
+}
---
+++ internal/puzzles/solutions/new_test.go
@@ -0,0 +1,65 @@
+package solutions
+
+import (
+	"testing"
+
+	"github.com/obalunenko/getenv"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_createNewFromTemplate(t *testing.T) {
+	purl := getenv.EnvOrDefault("AOC_PUZZLE_URL", "")
+	if purl == "" {
+		t.Skip("AOC_PUZZLE_URL is not set")
+	}
+
+	require.NoError(t, createNewFromTemplate(purl))
+}
+
+func Test_parsePuzzleURL(t *testing.T) {
+	type args struct {
+		url string
+	}
+
+	tests := []struct {
+		name     string
+		args     args
+		wandDate puzzleDate
+		wantErr  assert.ErrorAssertionFunc
+	}{
+		{
+			name: "valid url",
+			args: args{
+				url: "https://adventofcode.com/2022/day/1",
+			},
+			wandDate: puzzleDate{
+				year: 2022,
+				day:  1,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "invalid url",
+			args: args{
+				url: "https://adventofcode.com/2022",
+			},
+			wandDate: puzzleDate{
+				year: 0,
+				day:  0,
+			},
+			wantErr: assert.Error,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDate, err := parsePuzzleURL(tt.args.url)
+			if !tt.wantErr(t, err) {
+				return
+			}
+
+			assert.Equal(t, tt.wandDate, gotDate)
+		})
+	}
+}
